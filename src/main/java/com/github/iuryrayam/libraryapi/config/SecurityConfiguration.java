08b98b98e36c8afb4ba8f6d1f4051089573b006e
IuryRayam <iury2155@gmail.com>
2026-05-23 15:22:48 -0300

    create page web login

--- src/main/java/com/github/iuryrayam/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/com/github/iuryrayam/libraryapi/config/SecurityConfiguration.java
@@ -16,8 +16,10 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-//                .httpBasic(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(configure -> {
+                    configure.loginPage("/login").permitAll();
+                })
                 .authorizeHttpRequests(authorize ->{
                     authorize.anyRequest().authenticated();
                 })
